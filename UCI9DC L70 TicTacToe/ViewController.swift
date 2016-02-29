ca3a353fc1b74fd83ad41c2815e4daab580b4b29
Stanislav Sidelnikov <sidelnikov.stanislav@gmail.com>
2016-02-29 18:35:30 +0500

    Sync temporary progress

--- UCI9DC L70 TicTacToe/ViewController.swift
+++ UCI9DC L70 TicTacToe/ViewController.swift
@@ -9,10 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var crossNoughtsButtons: [UIButton]!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        for button in crossNoughtsButtons {
+            button.alpha = 0
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +24,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func buttonTap(sender: UIButton) {
+        print("Tapped \(sender.tag)")
+        sender.setImage(UIImage(named: "cross.png"), forState: .Normal)
+        UIView.animateWithDuration(0.5) { () -> Void in
+            sender.alpha = 1.0
+        }
+    }
 
 }
 
